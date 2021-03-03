4d79771cbcaab37cdb41c25486f7bda20e345292
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-03 18:15:12 -0300

    finalizandohandouts

--- get-it/database.py
+++ get-it/database.py
@@ -1,13 +1,26 @@
 import sqlite3
 from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class Note:
+    id: int = None
+    title: str = None
+    content: str = ''
+    
+def DB_PATH(DB_NAME):
+    try:
+        DB_NAME=str(DB_NAME)
+        if not DB_NAME.endswith('.db'):
+            return Path(DB_NAME + '.db')
+        else:
+            return Path(DB_NAME)
+    except:
+        raise ValueError("Did not enter a valid file name.")
 
 class Database:
     def __init__(self, DB_NAME):
-        if not DB_NAME.endswith('.db'):
-            self.DB_NAME=DB_NAME + '.db'
-        else:
-            self.DB_NAME=DB_NAME
-            
+        self.DB_NAME=DB_PATH(DB_NAME)
         self.conn=sqlite3.connect(self.DB_NAME)
         "CREATE TABLE IF NOT EXISTS dados_pessoais (nome_da_rua TEXT NOT NULL, cpf TEXT NOT NULL UNIQUE, identificador INTEGER PRIMARY KEY);"
         "INSERT INTO dados_pessoais (nome_da_rua,cpf) VALUES ('R. Quatá','123.456.789-00');"
@@ -15,12 +28,24 @@ class Database:
         self.conn.commit()
         
     def add(self, note):
-        self.id=note.id
-        self.title=note.title
-        self.content=note.content
-        
-@dataclass
-class Note:
-    id: int = None
-    title: str = None
-    content: str = ''
+        self.notation = f"INSERT INTO note (title, content) VALUES ('{note.title}','{note.content}');"
+        self.conn.execute(self.notation)
+        self.conn.commit()
+
+    def get_all(self,):
+        self.cursor = self.conn.execute("SELECT id, title, content FROM note")
+        self.note_list = []
+        for linha in self.cursor:
+            note_obj = Note(linha[0], linha[1], linha[2])
+            self.note_list.append(note_obj)
+        return self.note_list
+
+    def update(self, entry):
+        string_edition = f"UPDATE note SET title = '{entry.title}', content = '{entry.content}' WHERE id = {entry.id};"
+        self.cursor = self.conn.execute(string_edition)
+        self.conn.commit()
+
+    def delete(self, note_id):
+        delete_command = f"DELETE FROM note WHERE id = {note_id};"
+        self.cursor = self.conn.execute(delete_command)
+        self.conn.commit()
--- get-it/utils.py
+++ get-it/utils.py
@@ -4,9 +4,14 @@ import json
 def extract_route(string):
     return string.split()[1][1:]
 
+def is_path(subject):
+    if not type(subject) is Path:
+        return Path(subject)
+    else:
+        return subject
+        
 def read_file(path):
-    if not type(path) is Path:
-        path=Path(path)
+    path=is_path(path)
     extension=path.suffix
     if extension == '.txt' or extension == '.html' or extension == '.css' or extension == '.js':
         with open(path, 'rt') as file:
@@ -16,15 +21,32 @@ def read_file(path):
         with open(path, 'rb') as file:
             data=file.read()
         return data
+    
+def has_directory(string, directory):
+    c=0
+    for i in string:
+        if i == '/':
+            c+=1
+    if c>1:
+        return string
+    else:
+        return Path('./{0}/{1}'.format(directory, string))
 
 def load_data(path):
-    path='./data/'+path
+    path=has_directory(path, 'data')
     with open(path) as file: 
         data=json.load(file)
     return data
 
 def load_template(name):
-    path=Path('./templates/'+name)
+    path=has_directory(name, 'templates')
     return read_file(path)
 
 def build_response(body='', code=200, reason='OK', headers=''):
+    args = [str(code), reason]
+    response = 'HTTP/1.1 ' + (' '.join(args))
+    if headers == '':
+        response += '\n\n' + body
+    else:
+        response += '\n' + headers + '\n\n' + body
+    return response.encode()
