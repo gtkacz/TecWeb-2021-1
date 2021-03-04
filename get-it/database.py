7faf343923078d7dbfa35ebe9098313ab7c8c8bd
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-04 16:46:03 -0300

    database finalizado

--- get-it/database.py
+++ get-it/database.py
@@ -12,9 +12,9 @@ def DB_PATH(DB_NAME):
     try:
         DB_NAME=str(DB_NAME)
         if not DB_NAME.endswith('.db'):
-            return Path(DB_NAME + '.db')
+            return (DB_NAME + '.db')
         else:
-            return Path(DB_NAME)
+            return (DB_NAME)
     except:
         raise ValueError("Did not enter a valid file name.")
 
@@ -23,18 +23,18 @@ class Database:
         self.DB_NAME=DB_PATH(DB_NAME)
         self.conn=sqlite3.connect(self.DB_NAME)
         self.TABLE_NAME="note"
-        self.create=f'CREATE TABLE IF NOT EXISTS {self.TABLE_NAME} (id TEXT NOT NULL, title TEXT NOT NULL UNIQUE, content INTEGER PRIMARY KEY);'
-        self.conn.execute(self.create)
+        self.CREATE_ACTION=f'CREATE TABLE IF NOT EXISTS {self.TABLE_NAME} (id INTEGER PRIMARY KEY, title STRING, content STRING NOT NULL);'
+        self.conn.execute(self.CREATE_ACTION)
         self.conn.commit()
         
     def add(self, note):
-        self.ADD_ACTION = f'INSERT INTO {self.TABLE_NAME} (title, content) VALUES ({note.title},{note.content});'
+        self.ADD_ACTION = f'INSERT INTO {self.TABLE_NAME} (title, content) VALUES ("{note.title}","{note.content}");'
         self.conn.execute(self.ADD_ACTION)
         self.conn.commit()
         
     def get_all(self):
         self.select="SELECT id, title, content FROM note"
-        self.cursor = conn.execute(self.select)
+        self.cursor=self.conn.execute(self.select)
         self.READ_NOTE=[]
         for row in self.cursor:
             self.READ_NOTE.append(Note(row[0], row[1], row[2]))
@@ -42,13 +42,13 @@ class Database:
     
     def update(self, entry):
         if (entry.id is not None) and (entry.content is not None) and (entry.title is not None):
-            self.UPDATE_ACTION=f"UPDATE {self.TABLE_NAME} SET title = {entry.title}, content = {entry.content} WHERE id = {entry.id}"
+            self.UPDATE_ACTION=f"UPDATE {self.TABLE_NAME} SET title = '{entry.title}', content = '{entry.content}' WHERE id = {entry.id}"
             self.conn.execute(self.UPDATE_ACTION)
             self.conn.commit()
         else:
             raise TypeError("Function can't receive NoneType as argument.")
         
     def delete(self, note_id):
-        self.DELETE_ACTION=f"DELETE FROM {self.TABLE_NAME} WHERE {note_id};"
+        self.DELETE_ACTION=f"DELETE FROM {self.TABLE_NAME} WHERE id = {note_id};"
         self.conn.execute(self.DELETE_ACTION)
         self.conn.commit()
--- get-it/exemplo_de_uso.py
+++ get-it/exemplo_de_uso.py
@@ -1,6 +1,6 @@
 from database import Database, Note
 
-db = Database('banco')
+db = Database('banco-teste')
 db.add(Note(title='Pão doce', content='Abra o pão e coloque o seu suco em pó favorito.'))
 db.add(Note(title=None, content='Lembrar de tomar água'))
 notes = db.get_all()
